27b1c3d87a107656b9cfaf044a68dcbf81e1493d
sirajul musthafa <sirajulmusthafa786@gmail.com>
2026-08-05 15:12:22 +0530

    Ignore missing source map refs in whitenoise

--- flyingfox/settings.py
+++ flyingfox/settings.py
@@ -45,15 +45,14 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware', 
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'flyingfox.urls'
@@ -78,6 +77,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'flyingfox.wsgi.application'
+
+WHITENOISE_MANIFEST_STRICT = False
 
 
 # Database
