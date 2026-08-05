c83e6fd6785c9b2b38b9c3c26f94307eb59c09e0
sirajul musthafa <sirajulmusthafa786@gmail.com>
2026-08-05 15:17:28 +0530

    Switch to non-manifest whitenoise storage to fix missing source map crash

--- flyingfox/settings.py
+++ flyingfox/settings.py
@@ -138,15 +138,24 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+# STORAGES = {
+#     "default": {
+#         "BACKEND": "django.core.files.storage.FileSystemStorage",
+#     },
+#     "staticfiles": {
+#         "BACKEND": (
+#             "whitenoise.storage."
+#             "CompressedManifestStaticFilesStorage"
+#         ),
+#     },
+# }
+
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": (
-            "whitenoise.storage."
-            "CompressedManifestStaticFilesStorage"
-        ),
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
