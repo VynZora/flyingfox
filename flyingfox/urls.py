ce8751040ff53442ff15ab608b219d5832bd0a0b
sirajul musthafa <sirajulmusthafa786@gmail.com>
2026-09-03 15:31:56 +0530

    updated SEO updates

--- flyingfox/urls.py
+++ flyingfox/urls.py
@@ -1,33 +1,62 @@
 """
 URL configuration for flyingfox project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib.sitemaps.views import sitemap
+from django.views.generic import TemplateView
+
+from flyingfox_app.sitemaps import (
+    StaticViewSitemap,
+    RideSitemap,
+    BlogSitemap,
+    OfferSitemap,
+)
+
+
+sitemaps = {
+    "static": StaticViewSitemap,
+    "rides": RideSitemap,
+    "blogs": BlogSitemap,
+    "offers": OfferSitemap,
+}
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # Sitemap
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
+
+    # Robots.txt
+    path(
+        "robots.txt",
+        TemplateView.as_view(
+            template_name="robots.txt",
+            content_type="text/plain",
+        ),
+        name="robots_txt",
+    ),
+
+    # Main application
     path("", include("flyingfox_app.urls")),
 ]
 
+
 handler404 = "flyingfox_app.views.page_404"
 
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
---
+++ flyingfox_app/sitemaps.py
@@ -0,0 +1,102 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+from django.utils import timezone
+
+from .models import Ride, Blog, Offer
+
+
+# =========================================================
+# STATIC PAGES
+# =========================================================
+
+class StaticViewSitemap(Sitemap):
+    protocol = "https"
+    changefreq = "weekly"
+    priority = 0.8
+
+    def items(self):
+        return [
+            "home",
+            "rides",
+            "about",
+            "blog",
+            "offers",
+            "gallery",
+            "contact",
+            "terms_conditions",
+            "privacy_policy",
+        ]
+
+    def location(self, item):
+        return reverse(item)
+
+
+# =========================================================
+# RIDES
+# =========================================================
+
+class RideSitemap(Sitemap):
+    protocol = "https"
+    changefreq = "weekly"
+    priority = 0.9
+
+    def items(self):
+        return Ride.objects.filter(
+            is_active=True
+        )
+
+    def location(self, obj):
+        return reverse(
+            "ride_detail",
+            kwargs={
+                "slug": obj.slug
+            },
+        )
+
+
+# =========================================================
+# BLOGS
+# =========================================================
+
+class BlogSitemap(Sitemap):
+    protocol = "https"
+    changefreq = "weekly"
+    priority = 0.7
+
+    def items(self):
+        return Blog.objects.all()
+
+    def location(self, obj):
+        return reverse(
+            "blog_detail",
+            kwargs={
+                "slug": obj.slug
+            },
+        )
+
+
+# =========================================================
+# OFFERS
+# =========================================================
+
+class OfferSitemap(Sitemap):
+    protocol = "https"
+    changefreq = "daily"
+    priority = 0.7
+
+    def items(self):
+        today = timezone.localdate()
+
+        return Offer.objects.filter(
+            is_active=True,
+            start_date__lte=today,
+            end_date__gte=today,
+        )
+
+    def location(self, obj):
+        return reverse(
+            "frontend_offer_detail",
+            kwargs={
+                "slug": obj.slug
+            },
+        )
